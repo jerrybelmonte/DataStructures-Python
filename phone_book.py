b068c897a3d0431cb9b97399aa784688b171fb0e
Jerry Belmonte <gbelmont@ucsd.edu>
2020-12-03 18:22:58 -0800

    Add phone book implementation

--- phone_book.py
+++ phone_book.py
@@ -1,17 +1,43 @@
-# python3
+# Author: jerrybelmonte
 # Implement a simple phone book manager.
+
+
+class PhoneBook:
+    def __init__(self):
+        self.contacts = dict()
+
+    def add(self, number: int, name: str):
+        """Adds a contact to the phone book with the given name and number.
+        If the number already exists, the contact name will be overwritten."""
+        self.contacts[number] = name
+
+    def erase(self, number: int):
+        """Erases a person with the given number from the phone book if and
+        and only if the contact number is in the phone book."""
+        if number in self.contacts:
+            del self.contacts[number]
+
+    def find(self, number: int):
+        """Looks for a person with the given phone number. Returns a string
+        with the contact name, or 'not found' if the phone number is not in
+        the phone book."""
+        if number in self.contacts:
+            return self.contacts.get(number)
+        return 'not found'
+
 
 class Query:
     def __init__(self, query):
         self.type = query[0]
         self.number = int(query[1])
+        self.name = None
         if self.type == 'add':
             self.name = query[2]
 
 
 def read_queries():
     n = int(input())
-    return [Query(input().split()) for i in range(n)]
+    return [Query(input().split()) for _ in range(n)]
 
 
 def write_responses(result):
@@ -20,33 +46,18 @@ def write_responses(result):
 
 def process_queries(queries):
     result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    phone_book = PhoneBook()
+
     for cur_query in queries:
         if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
+            phone_book.add(cur_query.number, cur_query.name)
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
-        else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
+            phone_book.erase(cur_query.number)
+        elif cur_query.type == 'find':
+            result.append(phone_book.find(cur_query.number))
+
     return result
 
 
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
-
